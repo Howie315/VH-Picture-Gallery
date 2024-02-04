737adc0ad13a0c40c558195fb328b38ae5237aa4
Howie Nguyen <nguyen.howie2010@gmail.com>
2024-02-03 21:43:02 -0800

    added typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import "./features/HomePage/presentation/HomePage";
+import HomePage from "./features/HomePage/presentation/HomePage";
+
+function App() {
+	return <HomePage />;
+}
+
+export default App;
---
+++ frontend/src/features/HomePage/presentation/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomePage.scss";
+
+const HomePage = () => {
+	return (
+		<div className="home-page">
+			<h1>Welcome to the Home Page</h1>
+			<p>This is the starting point of our awesome React application.</p>
+		</div>
+	);
+};
+
+export default HomePage;
